d06e9f996eb6e0b16af4f3494df2e9d1544240a9
Cody Herriges <c.a.herriges@gmail.com>
2011-03-24 12:34:16 -0700

    Initial checkin.
      Should support package generation facts on Centos, Fedora, RHEL,
      Debian, Ubuntu, Solaris.

---
+++ lib/facter/pkg.rb
@@ -0,0 +1,44 @@
+# Cody Herriges <c.a.herriges@gmail.com>
+#
+# Testing the feasibility of generating a fact on the fly for every package
+# present on the system.
+
+case Facter.value(:operatingsystem)
+  when 'Debian', 'Ubuntu'
+    command = 'dpkg-query -W'
+    packages = []
+    Facter::Util::Resolution.exec(command).each_line do |pkg|
+      packages << pkg.chomp.split("\t")
+    end
+  when 'CentOS', 'RedHat', 'Fedora'
+    command = 'rpm -qa --qf %{NAME}"\t"%{VERSION}-%{RELEASE}"\n"'
+    packages = []
+    Facter::Util::Resolution.exec(command).each_line do |pkg|
+      packages << pkg.chomp.split("\t")
+    end
+  when 'Solaris'
+    command = 'pkginfo -x'
+    packages = []
+    combined = ''
+    Facter::Util::Resolution.exec(command).each_line do |line|
+      if line =~ /^\S/
+        then
+          combined << foo.chomp
+        else
+          combined << foo
+      end
+    end
+    combined.each_line do |pkg|
+      packages << pkg.chomp.scan(/^(\S+).*\s(\d.*)/)
+    end
+end
+
+packages.each { |key, value|
+
+  Facter.add(:"pkg_#{key}") {
+    setcode {
+      value
+    }
+  }
+
+}
